85c74481d185246f1da61147b57c2a1b520b330b
jraebrown <me@jraebrown.com>
2022-01-22 15:33:47 -0500

    Update apps.zsh

--- zshrc.d/apps.zsh
+++ zshrc.d/apps.zsh
@@ -51,11 +51,11 @@ alias brewup="brew update && brew upgrade"
 alias caskup="brew cu -af"
 export HOMEBREW_NO_ANALYTICS=1
 export HOMEBREW_CASK_OPTS="${HOMEBREW_CASK_OPTS:---appdir=/Applications}"
-export HOMEBREW_PREFIX="/opt/homebrew";
-export HOMEBREW_CELLAR="/opt/homebrew/Cellar";
-export HOMEBREW_REPOSITORY="/opt/homebrew";
-export MANPATH="/opt/homebrew/share/man${MANPATH+:$MANPATH}:";
-export INFOPATH="/opt/homebrew/share/info:${INFOPATH:-}";
+export HOMEBREW_PREFIX="/usr/local";
+export HOMEBREW_CELLAR="/usr/local/Cellar";
+export HOMEBREW_REPOSITORY="/usr/local/Homebrew";
+export MANPATH="/usr/local/share/man/man${MANPATH+:$MANPATH}:";
+export INFOPATH="/usr/local/share/info:${INFOPATH:-}";
 
 # java
 alias setjavahome="export JAVA_HOME=\`/usr/libexec/java_home\`"
